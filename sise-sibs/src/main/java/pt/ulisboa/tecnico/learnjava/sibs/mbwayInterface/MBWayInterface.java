1b0c68d305900f11899b3f955cef6773708bcceb
filipeajp <59914252+filipeajp@users.noreply.github.com>
2020-03-25 18:14:55 +0000

    v16.0

--- sise-sibs/src/main/java/pt/ulisboa/tecnico/learnjava/sibs/mbwayInterface/MBWayInterface.java
+++ sise-sibs/src/main/java/pt/ulisboa/tecnico/learnjava/sibs/mbwayInterface/MBWayInterface.java
@@ -18,47 +18,47 @@ import pt.ulisboa.tecnico.learnjava.sibs.exceptions.SibsException;
 public class MBWayInterface {
 	private static Client[] client;
 	private static Person[] person;
-	private static final int NR_CLIENTS = 2;
+	private static final int NR_CLIENTS = 10;
 
 	private static void createPersons() throws ClientException {
 		person = new Person[NR_CLIENTS];
 		person[0] = new Person("Filipe", "Pinheiro", "914801584", "First Street", 24);
 		person[1] = new Person("Joseph", "Manuel", "987654322", "Second Street", 34);
-//		person[2] = new Person("Joseph", "Manuel", "987654323", "Second Street", 34);
-//		person[3] = new Person("Joseph", "Manuel", "987654324", "Second Street", 34);
-//		person[4] = new Person("Joseph", "Manuel", "987654325", "Second Street", 34);
-//		person[5] = new Person("Joseph", "Manuel", "987654326", "Second Street", 34);
-//		person[6] = new Person("Joseph", "Manuel", "987654327", "Second Street", 34);
-//		person[7] = new Person("Joseph", "Manuel", "987654328", "Second Street", 34);
-//		person[8] = new Person("Joseph", "Manuel", "987654329", "Second Street", 34);
-//		person[9] = new Person("Joseph", "Manuel", "987654333", "Second Street", 34);
+		person[2] = new Person("Joseph", "Manuel", "987654323", "Second Street", 34);
+		person[3] = new Person("Joseph", "Manuel", "987654324", "Second Street", 34);
+		person[4] = new Person("Joseph", "Manuel", "987654325", "Second Street", 34);
+		person[5] = new Person("Joseph", "Manuel", "987654326", "Second Street", 34);
+		person[6] = new Person("Joseph", "Manuel", "987654327", "Second Street", 34);
+		person[7] = new Person("Joseph", "Manuel", "987654328", "Second Street", 34);
+		person[8] = new Person("Joseph", "Manuel", "987654329", "Second Street", 34);
+		person[9] = new Person("Joseph", "Manuel", "987654333", "Second Street", 34);
 	}
 
 	private static void createClients(Bank bank) throws ClientException {
 		client = new Client[NR_CLIENTS];
 		client[0] = new Client(bank, person[0], "242130704");
 		client[1] = new Client(bank, person[1], "123456789");
-//		client[2] = new Client(bank, person[2], "123456782");
-//		client[3] = new Client(bank, person[3], "123456783");
-//		client[4] = new Client(bank, person[4], "123456784");
-//		client[5] = new Client(bank, person[5], "123456785");
-//		client[6] = new Client(bank, person[6], "123456786");
-//		client[7] = new Client(bank, person[7], "123456787");
-//		client[8] = new Client(bank, person[8], "123456788");
-//		client[9] = new Client(bank, person[9], "123556789");
+		client[2] = new Client(bank, person[2], "123456782");
+		client[3] = new Client(bank, person[3], "123456783");
+		client[4] = new Client(bank, person[4], "123456784");
+		client[5] = new Client(bank, person[5], "123456785");
+		client[6] = new Client(bank, person[6], "123456786");
+		client[7] = new Client(bank, person[7], "123456787");
+		client[8] = new Client(bank, person[8], "123456788");
+		client[9] = new Client(bank, person[9], "123556789");
 	}
 
 	private static void createAccounts(Bank bank) throws BankException, AccountException, ClientException {
 		bank.createAccount(AccountType.CHECKING, client[0], 1000, 0);
 		bank.createAccount(AccountType.CHECKING, client[1], 1000, 0);
 		bank.createAccount(AccountType.CHECKING, client[2], 1000, 0);
-//		bank.createAccount(AccountType.CHECKING, client[3], 1000, 0);
-//		bank.createAccount(AccountType.CHECKING, client[4], 1000, 0);
-//		bank.createAccount(AccountType.CHECKING, client[5], 1000, 0);
-//		bank.createAccount(AccountType.CHECKING, client[6], 1000, 0);
-//		bank.createAccount(AccountType.CHECKING, client[7], 1000, 0);
-//		bank.createAccount(AccountType.CHECKING, client[8], 1000, 0);
-//		bank.createAccount(AccountType.CHECKING, client[9], 1000, 0);
+		bank.createAccount(AccountType.CHECKING, client[3], 1000, 0);
+		bank.createAccount(AccountType.CHECKING, client[4], 1000, 0);
+		bank.createAccount(AccountType.CHECKING, client[5], 1000, 0);
+		bank.createAccount(AccountType.CHECKING, client[6], 1000, 0);
+		bank.createAccount(AccountType.CHECKING, client[7], 1000, 0);
+		bank.createAccount(AccountType.CHECKING, client[8], 1000, 0);
+		bank.createAccount(AccountType.CHECKING, client[9], 1000, 0);
 	}
 
 	public static void main(String[] args) throws SibsException, AccountException, OperationException, BankException,
